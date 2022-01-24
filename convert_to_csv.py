984b62926a7e736f09cff4c040cf895cb669c266
nareshgn <gnnaresh@gmail.com>
2022-01-24 16:03:28 +0000

    read file by file

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -1,26 +1,26 @@
-# import pyspark as s
-# from pyspark import *
+import os
 import sys
+from pathlib import Path
+
 import pandas as pd
+import glob
 
-from utils.main_spark import read_files
+file_location = os.path.join('data', '*.xlsx')
+filenames = glob.glob(file_location)
 
-# from pyspark.sql import SparkSession
+for file in filenames:
+    df = pd.read_excel(file)
+    df = df.replace(['NULL'], '')
 
-# spark = SparkSession.builder.appName('PySpark Demo').getOrCreate()
-read_file = sys.argv[1]
-pd_df = pd.read_excel(read_file)
-# spark_df = spark.createDataFrame(pd_df.astype(str))
-# spark_df.coalesce(1).write.mode('overwrite').csv("data/", header=True, encoding='utf-8')
+    file_name = Path(file).stem
+    df.to_csv('data/'+file_name+'.csv', encoding='utf-8', index=False, header=True)
 
-pd_df.to_csv('data/csvfile.csv', encoding='utf-8', index=False, header=True)
-
-file_in = open("data/csvfile.csv", "rt")
-# output file to write the result to
-file_out = open("data/formatted_csvfile.csv", "wt")
-for line in file_in:
-    # read replace the string and write to output file
-    file_out.write(line.replace(',', '||'))
-# close input and output files
-file_in.close()
-file_out.close()
+    file_in = open('data/'+file_name+'.csv', 'rt')
+    # output file to write the result to
+    file_out = open('data/formatted_'+file_name+'.csv', "wt")
+    for line in file_in:
+        # read replace the string and write to output file
+        file_out.write(line.replace(',', '||'))
+    # close input and output files
+    file_in.close()
+    file_out.close()
